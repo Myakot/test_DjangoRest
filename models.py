c0734bcaee50b7e5ff17e4e81e5039588ac0e126
Myakot <cstepan@yandex.ru>
2024-10-31 16:35:59 +0700

    restored models.py

---
+++ models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Wallet(Base):
+    __tablename__ = 'wallets'
+    id = Column(String, primary_key=True)
+    balance = Column(Integer, default=0)
