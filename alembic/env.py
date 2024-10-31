f0af265dede087c53eeb25d3e1de8c20c855275a
Myakot <cstepan@yandex.ru>
2024-10-31 15:34:01 +0700

    set initial alembic migration + requirements.txt freezed from pip

--- alembic/env.py
+++ alembic/env.py
@@ -4,11 +4,15 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlalchemy import MetaData, create_engine
+from sqlalchemy.sql import text
 
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
+# thanks stackoverflow:
+# https://stackoverflow.com/questions/67526879/alembic-alembic-configuration-not-found-in-env-file
 config = context.config
 
 
@@ -18,6 +22,22 @@ section = config.config_ini_section
 config.set_section_option(section, "DB_USER", os.environ.get("DB_USER"))
 config.set_section_option(section, "DB_PASS", os.environ.get("DB_PASS"))
 
+metadata = MetaData()
+
+with open('db.sql', 'r') as f:
+    sql_code = f.read()
+
+for statement in sql_code.split(';'):
+    if statement.strip():
+        try:
+            query = text(statement)
+            engine = create_engine(config.get_main_option("sqlalchemy.url"))
+            query.compile(bind=engine)
+            metadata.reflect(bind=engine)
+        except Exception as e:
+            print(f"Ошибка при парсинге SQL-кода: {e}")
+
+print(metadata.tables) # Для теста, закомментить позже
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -28,7 +48,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
